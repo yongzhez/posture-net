54a9a589aee3cc09379220c257ee5436282ef667
Jason Zheng <yongzhe.zheng1@gmail.com>
2020-07-24 15:12:23 -0400

    setup basic error state

--- src/App.js
+++ src/App.js
@@ -4,16 +4,21 @@ import usePoseNet from './hooks/poseNetSetup';
 const Pose = () => {
   const videoRef = useRef();
   const [videoIsReady, setVideoIsReady] = useState(false);
+  const [videoError, setVideoError] = useState(null);
 
   useEffect(() => {
     navigator.mediaDevices.getUserMedia({ video: { facingMode: "environment" } })
       .then(stream => {
         videoRef.current.srcObject = stream;
       })
-      .catch(err => console.log(err))
+      .catch(err => setVideoError(err))
   },[]);
 
   usePoseNet({ videoRef, videoIsReady });
+
+  if (videoError) {
+    return <div>there was an error loading, make sure your browser supports webcam or that your webcam is turned on</div>
+  }
 
   return (
     <div>
--- src/hooks/poseNetSetup.js
+++ src/hooks/poseNetSetup.js
@@ -1,17 +1,18 @@
 import { useEffect  } from 'react';
 import * as posenet from "@tensorflow-models/posenet";
 
+const setupAndStartModel = async (videoRef) => {
+    const poseNetModel = await posenet.load();
+    const theThing = await poseNetModel.estimateSinglePose(videoRef.current, {
+      flipHorizontal: false
+    });
+    console.log(theThing);
+  }
+
 const usePoseNet = ({ videoRef, videoIsReady }) => {
     useEffect(() => {
         if (videoIsReady) {
-          const setupAndStartModel = async () => {
-            const poseNetModel = await posenet.load();
-            const theThing = await poseNetModel.estimateSinglePose(videoRef.current, {
-              flipHorizontal: false
-            });
-            console.log(theThing);
-          }
-          setupAndStartModel();
+          setupAndStartModel(videoRef);
         }
       },[videoRef, videoIsReady])
 }
