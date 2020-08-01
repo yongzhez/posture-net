b0db9e8d6fde0e98e7a867fe0ec198e77311b0b0
Jason Zheng <yongzhe.zheng1@gmail.com>
2020-08-01 12:02:42 -0400

    Updates

---
+++ precache-manifest.76ec52242eb33abc48a686b08d43ca4c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fc9631da1fecc5100b459a34f6f1c33",
+    "url": "/posture-net/build/index.html"
+  },
+  {
+    "revision": "115787512dc830359649",
+    "url": "/posture-net/build/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "d8f760c214fd304a5a98",
+    "url": "/posture-net/build/static/js/2.398be79d.chunk.js"
+  },
+  {
+    "revision": "203136b256d73b86d9b5f7bd77fb7e0d",
+    "url": "/posture-net/build/static/js/2.398be79d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "115787512dc830359649",
+    "url": "/posture-net/build/static/js/main.ca385918.chunk.js"
+  },
+  {
+    "revision": "9b844e9216936509e6e2",
+    "url": "/posture-net/build/static/js/runtime-main.525280de.js"
+  }
+]);
