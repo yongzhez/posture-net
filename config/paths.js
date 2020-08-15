588986cb44fa884c855c641ec8395a4eb6d04cca
Jason Zheng <yongzhe.zheng1@gmail.com>
2020-08-15 17:33:54 -0400

    add some other stuff

--- config/paths.js
+++ config/paths.js
@@ -56,6 +56,7 @@ module.exports = {
   appPublic: resolveApp('public'),
   appHtml: resolveApp('public/index.html'),
   appIndexJs: resolveModule(resolveApp, 'src/index'),
+  swHelloWorld: resolveModule(resolveApp, 'src/sw-hello-world'),
   appPackageJson: resolveApp('package.json'),
   appSrc: resolveApp('src'),
   appTsConfig: resolveApp('tsconfig.json'),
